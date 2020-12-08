5ba57088eb8f904365ddfe783b3175a468527bdf
Kviring Aleksey <alex@kviring.com>
2020-12-08 07:46:56 +0500

    Update bot markdown to v2

--- src/push/mod.rs
+++ src/push/mod.rs
@@ -1,5 +1,3 @@
-use std::fs;
-
 use crate::github::{PushEvent, Repository};
 
 pub fn process_push_event(event: String) -> String {
@@ -25,16 +23,32 @@ struct Commit {
     comment: String,
 }
 
+pub fn escape_markdown(source: &String) -> String {
+    let mut result = source.clone();
+    let chars = vec![
+        "*", "_", "{", "}", "[", "]", "(", ")", "#", "+", "-", ".", "!",
+    ];
+    chars.into_iter().for_each(|c| {
+        let to = format!("\\{}", c);
+        result = result.replace(c, to.as_str());
+    });
+    result
+}
+
 fn format_commit_message(author: String, repo: Repository, commits: Vec<Commit>) -> String {
     let concat = commits
         .iter()
-        .map(|c| format!("[➞]({}) {}\n", c.href, c.comment))
+        .map(|c| format!("[➞]({}) {}\n", c.href, escape_markdown(&c.comment)))
         .fold("".to_string(), |mut acc, g| {
             acc.push_str(&g.to_string());
             acc
         });
     return format!(
         "[{}](https://github.com/{}) push to [{}]({})\n {}",
-        author, author, repo.name, repo.html_url, concat
+        escape_markdown(&author),
+        author,
+        escape_markdown(&repo.name),
+        repo.html_url,
+        concat
     );
 }
