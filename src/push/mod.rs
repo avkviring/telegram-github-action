a1a34053c9d706357b26ba2100c05532cb3f9fdb
Kviring Aleksey <alex@kviring.com>
2020-12-21 07:08:33 +0500

    escaped >

--- src/push/mod.rs
+++ src/push/mod.rs
@@ -26,7 +26,7 @@ struct Commit {
 pub fn escape_markdown(source: &String) -> String {
     let mut result = source.clone();
     let chars = vec![
-        "*", "_", "{", "}", "[", "]", "(", ")", "#", "+", "-", ".", "!",
+        "*", "_", "{", "}", "[", "]", "(", ")", "#", "+", "-", ".", "!", ">",
     ];
     chars.into_iter().for_each(|c| {
         let to = format!("\\{}", c);
