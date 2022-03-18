d7ce742551ef275f83173a344f2662f3be8093e5
Lukyanenko Evgeny <juz@hive.ru>
2022-03-18 19:43:08 +0500

    wip: Прикрутил http сервер hyper, выводит POST data, отправленные на 127.0.0.1:3000/echo

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use std::env;
 use crate::event::push::process_push_event;
 use crate::event::release::process_release_event;
 use crate::telegram::send_message_to_telegram;
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Method, StatusCode};
 
 pub mod event;
 pub mod markdown;
@@ -16,17 +21,38 @@ pub mod test;
 
 #[tokio::main]
 async fn main() {
-    let event = env::var("event").expect("Add environment event:${{ github.event }}");
-    let message = if event.contains("\"release\"") {
-        process_release_event(event.to_string())
-    } else {
-        process_push_event(event.to_string())
+    // We'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }}
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+
+    match (_req.method(), _req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try POSTing data to /echo");
+        },
+        (&Method::POST, "/echo") => {
+            println!("hello there!");
+            *response.body_mut() = _req.into_body();
+        },
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        },
     };
 
-    let token = env::var("telegram_token")
-        .expect("Add environment telegram_token:${{ secrets.telegram_token }}");
-    let chat_id =
-        env::var("telegram_to").expect("Add environment telegram_to:${{ secrets.telegram_to }}");
-
-    send_message_to_telegram(token, chat_id, message).await;
+    Ok(response)
 }
