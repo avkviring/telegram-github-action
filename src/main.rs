17ef0a4dc85fcf8abf6eb3a38ab6bb67b3b9fc54
Lukyanenko Evgeny <juz@hive.ru>
2022-03-22 18:40:57 +0500

    wip: получаю push ивент из гитлаба через хук и вывожу заголовки и тело запроса в консоль.

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ use crate::event::release::process_release_event;
 use crate::telegram::send_message_to_telegram;
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use futures::{future, TryStreamExt};
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Method, StatusCode};
+use tokio::stream::StreamExt;
+use hyper::body::HttpBody;
+use hyper::body::to_bytes;
 
 pub mod event;
 pub mod markdown;
@@ -22,7 +26,7 @@ pub mod test;
 #[tokio::main]
 async fn main() {
     // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
     // A `Service` is needed for every connection, so this
     // creates one from our `hello_world` function.
@@ -36,7 +40,8 @@ async fn main() {
     // Run this server for... forever!
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
-    }}
+    }
+}
 
 async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
@@ -44,14 +49,21 @@ async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> 
     match (_req.method(), _req.uri().path()) {
         (&Method::GET, "/") => {
             *response.body_mut() = Body::from("Try POSTing data to /echo");
-        },
+        }
         (&Method::POST, "/echo") => {
-            println!("hello there!");
-            *response.body_mut() = _req.into_body();
-        },
+            println!("hello");
+
+            for h in _req.headers() {
+                println!("{} = {}", h.0, h.1.to_str().unwrap());
+            }
+
+            let vec = _req.into_body().data().await.unwrap().unwrap().to_vec();
+            println!("{}", std::str::from_utf8(&vec).unwrap());
+            // *response.body_mut() = _req.into_body();
+        }
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
-        },
+        }
     };
 
     Ok(response)
